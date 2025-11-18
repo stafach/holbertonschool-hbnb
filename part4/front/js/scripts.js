b4e8db7ea38d3b008b0ed89d1074b7aa7d6ea6a5
chermat mustapha <mchermat@outlook.fr>
2025-11-18 13:50:43 +0100

    finish task 2, add all places to index.html and display them, add a price selector

--- part4/front/js/scripts.js
+++ part4/front/js/scripts.js
@@ -1,3 +1,4 @@
+/// Request for login
 async function loginUser(email, password) {
     const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
         method: 'POST',
@@ -9,6 +10,7 @@ async function loginUser(email, password) {
     return response;
 }
 
+/// Try to login
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
 
@@ -36,4 +38,88 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+});
+
+/// Fetch places data
+async function fetchPlaces(token) {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/api/v1/places/', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const places = await response.json();
+    return places;
+  } catch (error) {
+    console.error("Error fetching places :", error);
+    return [];
+  }
+}
+
+/// Fetch the token in the cookie
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+
+/// Check user authentication
+async function checkAuthentication() {
+    const token = getCookie('token');
+    const loginLink = document.getElementsByClassName('login-button');
+
+    for (const link of loginLink) {
+        link.style.display = token ? 'none' : 'block';
+    }
+
+    if (token) {
+        // Fetch places data if the user is authenticated
+        const places = await fetchPlaces(token);
+
+        /// Populate places list
+        place_list = document.getElementById('places-list');
+        place_list.innerHTML = "";
+        
+        places.forEach(place => {
+            const card = document.createElement('div');
+            card.classList.add('place-card');
+            card.innerHTML = `
+                <h3>Name</h3>
+                <p>${place.title}</p>
+                <h3>Price</h3>
+                <p data-price="${place.price}">${place.price}</p>
+                <h3>Description</h3>
+                <p>${place.description}</p>
+                <h3>Position</h3>
+                <p>${place.latitude} ${place.longitude}</p>
+            `;
+            place_list.appendChild(card);
+        });
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    checkAuthentication();
+});
+
+document.getElementById('price-filter').addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+
+    const placeCards = document.querySelectorAll('.place-card');
+    placeCards.forEach(card => {
+        const price = parseFloat(card.querySelector('p[data-price]').dataset.price);
+
+        if (selectedValue === 'All' || price <= parseFloat(selectedValue)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
 });
